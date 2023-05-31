52489a0108c7e6fe2410ded3fd530a3a49ab399d
Jess <jessica.louvel@gmail.com>
2023-05-31 16:22:32 +0200

    Etape 1

---
+++ backend/routes/books.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+const booksController = require('../controllers/books');
+
+router.get('/', booksController.getAllBooks);
+router.get('/:id', booksController.getOneBook);
+router.get('/bestrating', booksController.getBestRate);
+router.post('/', auth, multer, booksController.createBook);
+router.put('/:id', auth, multer, booksController.modifyBook);
+router.delete('/:id', auth, booksController.deleteBook);
+router.post('/:id/rating', auth, booksController.rateBook);
+
+module.exports = router;
